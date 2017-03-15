99573631ac5566ac1a8fd5310776006a9ff92cf9
liuliyang186 <2865144060@qq.com>
2017-03-15 14:30:58 +0800

    修改

--- aboutUs.js
+++ aboutUs.js
@@ -1,6 +1,4 @@
-/**
- * Created by dell on 2017/3/13.
- */
+
 $(function(){
     $.get("/resources/json/aboutus.json",{},function(data){
         $(".aboutus-main").html(template('aboutus-main-template',data));
